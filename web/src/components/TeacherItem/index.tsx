0e680b938157fbe28f5a2971acb747b37b3cd2cc
aabastos <andrebastos1198@gmail.com>
2020-08-09 18:44:42 -0300

    finalização do web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,39 +4,40 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-interface TeacherItemProps {
+interface Teacher {
+    name: string,
+    avatar: string,
+    whatsapp: string
+    bio: string,
+    subject: string,
+    cost: number
+}
 
+interface TeacherItemProps {
+    index: number,
+    teacher: Teacher
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = (props) => {
     return (
-        <article className="teacher-item">
+        <article key={props.index} className="teacher-item">
             <header>
-                <img src="https://pbs.twimg.com/profile_images/1214328185732706304/aQcD5wGd_400x400.jpg" alt="André Bastos" />
+                <img src={props.teacher.avatar} alt={props.teacher.name} />
                 <div>
-                    <strong>André Bastos</strong>
-                    <span>JavaScript</span>
+                    <strong>{props.teacher.name}</strong>
+                    <span>{props.teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                <br /><br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <p>{props.teacher.bio}</p>
 
             <footer>
-                <p>
-                    Preço/hora
-                            <strong>
-                        R$20,00
-                            </strong>
-                </p>
+                <p>Preço/hora<strong>R${props.teacher.cost}</strong></p>
 
-                <button type="button">
+                <a target="__blank" href={`https://wa.me/${props.teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp" />
-                            Entrar em contato
-                        </button>
+                    Entrar em contato
+                </a>
             </footer>
         </article>
     )
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,35 +1,91 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
+import api from '../../services/api';
 
 function TeacherList() {
+    interface Teacher {
+        user_id: number,
+        name: string,
+        avatar: string,
+        whatsapp: string,
+        bio: string,
+        subject: string,
+        cost: number
+    }
+
+    const [subject, setSubject] = useState("");
+    const [week_day, setWeekDay] = useState("");
+    const [time, setTime] = useState("");
+
+    const [teachers, setTeachers] = useState([]);
+
+    function searchTeachers(e: FormEvent) {
+        e.preventDefault();
+
+        api.get('/classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        }).then(response => {
+            const teachers = response.data;
+            setTeachers(teachers);
+        });
+
+    }
+
     return (
         <div id="page-teacher-list">
             <PageHeader title="Estes são os proffys disponíveis." >
-                <form id="search-teachers">
+                <form id="search-teachers" onSubmit={searchTeachers}>
                     <div className="input-block">
                         <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
+                        <select id="subject" name="subject" value={subject} onChange={(e) => { setSubject(e.target.value) }}>
+                            <option hidden disabled value="">Selecione uma opção</option>
+                            <option value="JavaScript">JavaScript</option>
+                            <option value="TypeScript">TypeScript</option>
+                            <option value="React Native">React Native</option>
+                            <option value="ReactJS">ReactJS</option>
+                            <option value="NodeJS">NodeJS</option>
+                        </select>
                     </div>
 
                     <div className="input-block">
                         <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day" />
+                        <select id="week_day" name="week_day" value={week_day} onChange={(e) => setWeekDay(e.target.value)}>
+                            <option hidden disabled value="9">Selecione uma opção</option>
+                            <option value="0" label="Domingo" />
+                            <option value="1" label="Segunda-Feira" />
+                            <option value="2" label="Terça-Feira" />
+                            <option value="3" label="Quarta-Feira" />
+                            <option value="4" label="Quinta-Feira" />
+                            <option value="5" label="Sexta-Feira" />
+                            <option value="6" label="Sábado" />
+                        </select>
                     </div>
 
                     <div className="input-block">
                         <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
+                        <input type="time" id="from" name="from" value={time} onChange={(e) => setTime(e.target.value)} />
                     </div>
+
+                    <button type="submit">Buscar</button>
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem />
-                <TeacherItem />
+                {
+                    teachers.map((teacher: Teacher, index) => {
+                        return (
+                            <TeacherItem index={index} teacher={teacher} />
+                        );
+                    })
+                }
             </main>
         </div>
     )
