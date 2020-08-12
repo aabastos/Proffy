557055c58823b0ff85d468ae1854ec8ed20ba972
aabastos <andrebastos1198@gmail.com>
2020-08-11 23:26:17 -0300

    estruturação da landing page

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 
 import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 
 import styles from './styles';
 
@@ -13,6 +15,17 @@ function Landing() {
                 Seja bem-vindo, {'\n'}
                 <Text style={styles.titleBold}>O que deseja fazer?</Text>
             </Text>
+
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={studyIcon} />
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon} />
+                    <Text style={styles.buttonText}>Dar aulas</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
